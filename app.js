45a753c1e5b9c203587ffb372d152fa20f957da2
ahsansamad <ingeniouslogo@gmail.com>
2024-05-29 14:55:40 +0500

    S

--- app.js
+++ app.js
@@ -2,30 +2,46 @@
 
 
 
-function myFunc(value){
-    var matchingBrackets = {
-        ')': '(',
-        '}': '{',
-        ']': '['
-    }
-    var input = document.getElementById('input')
-    var sett = []
-    for(i = 0 ; i < input.value.length; i++){
-        var brack = input.value[i]
-        // console.log(brack)
-        if(brack == '[' || brack == '{' || brack == '('){
-            sett.push(brack)
-            console.log(sett)
-        }
-        else if(
-            brack == '}' || brack == ')' || brack == "]"
-        ){
-            sett.pop()
-            if(sett == 0){
-                console.log("balance")
-            }
+// function myFunc(value){
+  
+//     var input = document.getElementById('input')
+//     var sett = []
+// //     for(i = 0 ; i < input.value.length; i++){
+// //         var brack = input.value[i]
+// //         // console.log(brack)
+// //         if(brack == '[' || brack == '{' || brack == '('){
+// //             sett.push(brack)
+// //             console.log(sett)
+// //         }
+// //         else if(
+// //             brack == '}' || brack == ')' || brack == "]"
+// //         ){
+// //             sett.pop()
+// //             if(sett == 0){
+// //                 console.log("balance")
+// //             }
 
-        }
-    }
-
-}
+// //         }
+// //     }
+// for( var i = 0 ; i < input.value.length; i++){
+//     var brack = input.value[i]
+//     if(brack == "(" || brack == "[" || brack == "{"){
+//         sett.push(brack[i])
+//         console.log("balanced")
+//     }
+//     if(brack == ")" || brack == "]" || brack == "}"){
+//         sett.pop()
+//         if(sett.length == 0){
+            
+//             console.log("unbalanced")
+//         }
+//     }
+//     }
+//     // if(sett.length == 0){
+//     // console.log("balance")
+//     // }
+//     // else{
+//     //     console.log("unbalanced")
+//     // }
+    
+// }
